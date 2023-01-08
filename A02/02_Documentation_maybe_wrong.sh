4f8c0f618d47a9d003f499e92cbf6b7589aae53f
Tom Wechsler <tom.wechsler@outlook.com>
2023-01-08 16:39:27 +0100

    lines edited

--- A02/02_Documentation_maybe_wrong.sh
+++ A02/02_Documentation_maybe_wrong.sh
@@ -20,4 +20,5 @@ openssl passwd -1 -salt mysalt Password1
 When setting user passwords programmatically, always use random SALT
 Lets investigate passwords, salt and authentication.
 
+
 https://docs.ansible.com/ansible/latest/reference_appendices/faq.html#how-do-i-generate-encrypted-passwords-for-the-user-module
