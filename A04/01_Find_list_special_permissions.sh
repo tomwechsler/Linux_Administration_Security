89286b832e5b9e47b5c03ca5eb890465892a439e
Tom Wechsler <tom.wechsler@outlook.com>
2023-01-09 13:21:44 +0100

    new files

---
+++ A04/01_Find_list_special_permissions.sh
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+
+SUID
+Used on programs to run as the user owner during execution
+
+SGID
+On directories, new files are assigned the group owner from the directory
+
+Sticky Bit
+With this set users can only delete files they own from shared directories
