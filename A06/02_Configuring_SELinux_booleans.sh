0a663606a8ec53d377bff4070d9dc96226a2b568
Tom Wechsler <tom.wechsler@outlook.com>
2023-02-01 06:49:47 +0100

    new files

--- A06/02_Configuring_SELinux_booleans.sh
+++ A06/02_Configuring_SELinux_booleans.sh
@@ -5,9 +5,30 @@ getsebool -a
 
 getsebool zoneminder_run_sudo
 
+getsebool -a | grep policy
 
-semanage boolean --list
-setsebool secure_mode_policyload on (-P)
+#Install SELinux Utilities (setroubleshoot adds log information in /var/log/messages)
+sudo dnf install -y policycoreutils setools setools-console setroubleshoot
+
+#Get a list
+sudo semanage boolean --list
+
+sudo semanage boolean --list | head
+
+#Search for policy
+sudo semanage boolean --list | grep policy
+
+#Change the value
+sudo setsebool secure_mode_policyload on #setsebool secure_mode_policyload on (-P)
+
+#Search for policy
+sudo semanage boolean --list | grep policy
+
+#Check SELinux
+getenforce
+
+#To change SELinux at this time will not work anymore
+sudo setenforce 0
 
 
 
