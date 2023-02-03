3a037150cf7e83636a18c236a15e2bafb8eefa7a
Tom Wechsler <tom.wechsler@outlook.com>
2023-02-03 06:48:44 +0100

    new files

--- A06/04_Relocating_user_home_directories.sh
+++ A06/04_Relocating_user_home_directories.sh
@@ -1,19 +1,39 @@
 #Working on alma
 
-sudo ls /etc/selinux/targeted/contexts/files
-
+#Check the man pages
 man semanage-fcontext
 
+#The location for context files
+sudo ls /etc/selinux/targeted/contexts/files
+
+#List the SELinux metadata
+ls -dZ /home
+
+ls -dZ /home/vagrant
+
+#Create a new directory for new user homes
 sudo mkdir /staff
-
-sudo semanage fcontext -a -e /home /staff
-
-sudo restorecon -v /staff
 
 ls -ldZ /staff
 
+#Set the SELinux context
+sudo semanage fcontext -a -e /home /staff
+
+#If we get an error - this is because of the boolean policy, just reboot
+
+#Erease the SELinux context
+sudo semanage fcontext -d /staff
+
+#Set it again
+sudo semanage fcontext -a -e /home /staff
+
+#Restore it
+sudo restorecon -v /staff
+
+#Create a user
 sudo useradd -m -d /staff/u1 u1
 
+#Check SELiunx
 sudo ls -ldZ /staff/u1
 
 
