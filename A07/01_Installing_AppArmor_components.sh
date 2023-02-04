7b43bbdabff10eb0bfcf11eb3a4d3348e0b6a555
Tom Wechsler <tom.wechsler@outlook.com>
2023-02-04 07:12:58 +0100

    new files

---
+++ A07/01_Installing_AppArmor_components.sh
@@ -0,0 +1,32 @@
+#Working on opensuse
+
+#Are there any AppArmor tools
+aa- TABTAB
+
+#Install the tools
+zypper in -y apparmor-utils
+
+#Check again
+aa- TABTAB
+
+aa-status
+
+#Lets install the profiles
+zypper in -y apparmor-profiles
+
+#Check the service
+rcapparmor status
+
+rcapparmor start
+
+rcapparmor status
+
+systemctl status apparmor
+
+#Show the status
+aa-status
+
+
+
+Adding the tools will enable AppArmor on your system but without profiles. Adding
+profiles add targets to secure.
