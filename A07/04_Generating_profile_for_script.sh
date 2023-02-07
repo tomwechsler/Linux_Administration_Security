a3c6b40e1a9c1e6302829b294650332b3afee4ea
Tom Wechsler <tom.wechsler@outlook.com>
2023-02-07 06:56:25 +0100

    new files

--- A07/04_Generating_profile_for_script.sh
+++ A07/04_Generating_profile_for_script.sh
@@ -1,4 +1,4 @@
-#Working on opensuse
+#Working on opensuse (two ssh session)
 
 #Start a root session
 sudo su -
@@ -6,3 +6,22 @@ sudo su -
 #Install rsyslog
 zypper install -y rsyslog
 
+#Generate a profile
+aa-genprof test.py
+
+#Switch to the other session
+./test.py
+
+#Back to the other session
+Type S (Scan)
+
+Type A (Allow)
+
+Type S (Save)
+
+Type F (Finish)
+
+#Switch to the other session
+./test.py
+#Run again (it works fine)
+./test.py
