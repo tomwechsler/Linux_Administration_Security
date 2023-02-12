a545fe9687b4826a771327f6c466060abd66f501
Tom Wechsler <tom.wechsler@outlook.com>
2023-02-12 15:28:53 +0100

    new files

--- A09/01_Using_su.sh
+++ A09/01_Using_su.sh
@@ -1,4 +1,4 @@
-#Working on alma with the following prep steps
+#Working on alma with the following preperation steps
 
 sudo passwd vagrant
 sudo usermod .aG wheel vagrant
@@ -8,14 +8,55 @@ vagrant ssh alma
 #What if have done
 history
 
+#Set the root password (the vagrant box comes without root pw)
+sudo passwd root
 
+#Substitute user (no complete login shell)
 su
 
+#The environment
+pwd
+
+id
+
+echo $USER
+
+ps
+
+exit
+
+#Check with ps
+ps
+
+#A full login
 su -
 
-su -l
+pwd
 
+echo $USER
+
+#Create the a user
+useradd -m Bob -s /bin/bash
+
+#Switch to Bob without a password
 su - bob
+
+pwd
+
+exit
+
+exit
+
+#Switch to Bob without a password
+sudo su - bob
+
+exit
+
+#Lock the root account (for security)
+sudo passwd -l root
+
+#Check
+su - root
 
 
 
