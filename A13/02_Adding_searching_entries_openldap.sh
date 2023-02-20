932e27f5892e49e0084e70daf552a1fb040f964b
Tom Wechsler <tom.wechsler@outlook.com>
2023-02-20 14:23:05 +0100

    lines edited

--- A13/02_Adding_searching_entries_openldap.sh
+++ A13/02_Adding_searching_entries_openldap.sh
@@ -11,9 +11,9 @@ vim user.ldif
 ldapadd -x -D cn=admin,dc=example,dc=com -W -f user.ldif # -x Use simple authentication; -D Use the Distinguished Name; -W Prompt for simple authentication; -f file
 
 #Search
-ldapsearch -x -b dc=example, dc=com '(objectClass=inetorgPerson)' cn
+ldapsearch -x -b dc=example,dc=com '(objectClass=inetorgPerson)' cn
 
-ldapsearch -x -b dc=example, dc=com '(objectClass=posixGroup)'
+ldapsearch -x -b dc=example,dc=com '(objectClass=posixGroup)'
 
 #Set the password for the user (Attention first we hav to set the user password, after that we enter the admin password)
 ldappasswd -x -D cn=admin,dc=example,dc=com -S uid=john,ou=People,dc=example,dc=com -W   #-S Prompt for new password; -W Prompt for bind password
