ad0e2856e346c6acced20f0c95fd72e1449c669f
Tom Wechsler <tom.wechsler@outlook.com>
2023-02-21 14:46:45 +0100

    lines edited

--- A16/03_Allowing_HTTP_access.sh
+++ A16/03_Allowing_HTTP_access.sh
@@ -3,7 +3,7 @@
 sudo -i
 
 #Install apache2
-sudo apt update
+apt update
 
 apt install -y apache2
 
@@ -11,7 +11,7 @@ apt install -y apache2
 curl 192.168.56.102 #It works not
 
 #Set a allow rule
-ufw enable from 192.168.56.101 to any port http
+ufw allow from 192.168.56.101 to any port http
 
 #Check the rules
 ufw status
