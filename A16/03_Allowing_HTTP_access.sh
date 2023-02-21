4508dba2708fb4f1c2754df001e04fefd75597d7
Tom Wechsler <tom.wechsler@outlook.com>
2023-02-21 07:06:03 +0100

    new files

---
+++ A16/03_Allowing_HTTP_access.sh
@@ -0,0 +1,26 @@
+#Working on ubuntu in a root session
+
+sudo -i
+
+#Install apache2
+sudo apt update
+
+apt install -y apache2
+
+#SWITCH TO ALMA
+curl 192.168.56.102 #It works not
+
+#Set a allow rule
+ufw enable from 192.168.56.101 to any port http
+
+#Check the rules
+ufw status
+
+#SWITCH TO ALMA
+curl 192.168.56.102 #It works
+
+
+
+
+
+We can enable HTTP access from the AlmaLinux system.
