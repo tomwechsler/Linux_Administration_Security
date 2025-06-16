0bb2e3d2f79db8973badbe51e8af8311dac0bee4
Tom Wechsler <tom.wechsler@outlook.com>
2025-06-16 14:28:41 +0200

    some corrections

--- A10/05_Working_passwords_understanding_authentication.sh
+++ A10/05_Working_passwords_understanding_authentication.sh
@@ -5,6 +5,9 @@ sudo -i
 #The vagrant user (the values between $)
 sudo getent shadow vagrant
 
+#Get the encrypted password field for 'vagrant' user from the shadow database with root privileges
+sudo getent shadow vagrant | cut -f2 -d:
+
 #Lets look a bit closer
 awk -F$ '/vagrant/ { print "alg: " $2 "\nsalt: " $3 "\nPassword: " $4 }' /etc/shadow
 
@@ -20,13 +23,3 @@ openssl passwd -6 P@ssword
 openssl passwd -6 P@ssword
 
 openssl passwd -6 P@ssword
-
-
-
-
-Passwords are stored within the second field of the shadow file. The entry itself is broken down
-3 separate entities: the algorithm, the SALT and the password hash.
-
-We can quickli create a user account setting its password. RedHat based systems have the
-option --stdin but Debian based systems do not. We can show the authentication process by
-using the openssl command
